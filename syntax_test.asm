987ec4e138fbee2cd6a711a4f77c5aa87deb0282
blackrockjet <menadue@gmail.com>
2023-08-26 09:00:36 +0100

    Updates

--- syntax_test.asm
+++ syntax_test.asm
@@ -7,6 +7,10 @@
 	.INCLUDE        MOSHEAD.INC
 	.INCLUDE        MSWI.INC
 
+VALUE1  .EQU  		10	
+VALUE2  .EQU  		20
+VALUE3  .EQU  		30	
+	
 	;; ----------------------------------------
 	;; Macro syntax
 	;; ----------------------------------------
@@ -26,7 +30,7 @@ BCLR    .macro	mask, addr
 	;; Decimal
 	LDAA    #12
 
-	;; $ hex
+	;; $ hex 
 	LDAA    #$12
 
 	;; Psion style hex
@@ -39,6 +43,28 @@ BCLR    .macro	mask, addr
 	;; floating point expressions
 
 	LDAA    #int(100*sin(1))
+
+	LDAA    #(1<<6)
+	LDAA	#int(3*log(exp(1)))
+
+	LDAA    #[set ::PASS]
+
+	;; equates
+	LDAA    #VALUE1
+	LDAA    #VALUE1 + VALUE2
+	LDAA    #VALUE1/4
+
+	;; Psion operators
+	;; Bit masks using a number
+	
+	LDAA    #^B=5=
+	LDAA    #^B?5?
+
+MASK1   .EQU    (1<<1)
+MASK2   .EQU    (1<<2)
+
+	LDAA    #MASK1 | MASK2
+	
 	
 	
 	
